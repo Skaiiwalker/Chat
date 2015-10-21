75a67e01e0cdb91bd7bc88a0ca302b64bb470930
Skaiiwalker <lbar8299@740s-pl-132.local>
2015-10-21 09:12:23 -0600

    Chatbot
    chatbot

--- src/chat/controller/ChatController.java
+++ src/chat/controller/ChatController.java
@@ -1,9 +1,18 @@
 package chat.controller;
 
+import chat.view.ChatView;
+
+/**
+ * Application controller for the Chatbot project.
+ * @author Luke Barrowes
+ *@version 1.1.1 10/21/15 fixed error :D
+ */
 public class ChatController
+{
+	private ChatView myDisplay;
+	
+	public void start()
 	{
-		public void start()
-		{
-			
-		}
+		
 	}
+}
